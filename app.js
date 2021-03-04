3d5a2e2597fc82e9ab649fa9ed4d6e7371b613c7
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-03-04 13:38:30 +0200

    js

---
+++ app.js
@@ -0,0 +1,38 @@
+var user_name = prompt("Please enter your name");
+    alert("case sinsitive!!");
+    var user_color = prompt("please choose yor color = red,green,blue ");
+      
+    confirm("are you sure thats your name ?");
+console.log("HIs from ASAC ");
+document.write("Hi from ASAC \n");
+
+document.write("\n");
+
+
+if (user_color=="red"){document.write("red")}
+else if (user_color=="green"){document.write("green")}
+else if (user_color=="blue"){document.write("blue")}
+
+
+
+
+        
+document.write("\n");
+      
+        var animal = prompt("Please enter an animal");
+        if (animal == "cat")
+        {
+         document.write(animal);
+        }
+        else if(animal == "dog")
+        {
+                alert("you entered a dog");
+        }
+      else 
+      {
+              alert("not matched");
+      }
+      var userr_name = "mahmood";
+      var sec_name = "alashqar";
+      var Full_Name = userr_name + sec_name;
+      document.write(Full_Name)
